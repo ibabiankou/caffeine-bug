ae456618a98c616b44b30a7d29f5a3a2dd170093
Ivan Babiankou <ivan.babiankou@gmail.com>
2021-11-23 14:30:31 +0100

    Support macos terminal

--- parallel.sh
+++ parallel.sh
@@ -4,6 +4,19 @@ mvn clean package
 
 for i in $(seq 1 ${1:-1}); do
   name="terminal$i"
-	guake -n "$(pwd)" -r "$name" -e "./run.sh ${2:-1} $i"
-done
 
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux"
+    guake -n "$(pwd)" -r "$name" -e "./run.sh ${2:-1} $i"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Macos"
+    script="./run.sh ${2:-1} $i"
+    osascript \
+      -e 'tell application "Terminal" to activate' \
+      -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
+      -e "tell application \"Terminal\" to do script \"$script\" in selected tab of the front window"
+    sleep 3
+  else
+    echo "Unsupported OS"
+  fi
+done
